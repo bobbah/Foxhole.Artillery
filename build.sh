dac864f0416eaa7e6fa2414b29aea7f83bb55f48
Brett <hello@brett.gg>
2023-11-04 22:53:24 -0300

    add AI generated:tm: modifications for fixing shallow clones on cloudflare

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
+# Ensure we're in a git repository
+if [ ! -d ".git" ]; then
+  echo "This directory does not seem to be a Git repository."
+  exit 1
+fi
+
+# Fetch the full repository history
+git fetch --unshallow || {
+  echo "Notice: It seems the repository was already unshallowed or fetching the full history is not possible."
+}
+
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0
