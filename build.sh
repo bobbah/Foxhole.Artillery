ef14e2449e52e627704371699163c65601c6e0ad
Brett <hello@brett.gg>
2023-11-04 22:33:53 -0300

    you know what? the documentation will be OVERCOME.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet publish Foxhole.Artillery -o output -c Release
+./dotnet-install.sh -c 7.0
+dotnet --version
+dotnet publish Foxhole.Artillery -o output -c Release
